3892c0bddeda47eae0b44d32ecfbee3c33d8a5e1
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-04-01 15:52:03 +0200

    Setup GameLogic

--- Minesweeper/ContentView.swift
+++ Minesweeper/ContentView.swift
@@ -14,12 +14,34 @@ struct ContentView: View {
         rows.flatMap { $0 }
     }
 
+    var revealedSquares: [Square] {
+        allSquares.filter(\.isRevealed)
+    }
+
+    var flaggedSquares: [Square] {
+        allSquares.filter(\.isFlagged)
+    }
+
+    var minedSquares: [Square] {
+        allSquares.filter(\.hasMine)
+    }
+
+    var minesFound: Int {
+        max(0, minedSquares.count - flaggedSquares.count)
+    }
+
     var body: some View {
         Grid(horizontalSpacing: 2, verticalSpacing: 2) {
             ForEach(0..<rows.count, id: \.self) { row in
                 GridRow {
                     ForEach(rows[row]) { square in
                         SquareView(square: square)
+                            .onTapGesture {
+                                select(square)
+                            }
+                            .onLongPressGesture {
+                                flag(square)
+                            }
                     }
                 }
             }
@@ -84,6 +106,46 @@ struct ContentView: View {
                 square.nearbyMines = adjacentSquares.filter(\.hasMine).count
             }
         }
+    }
+
+    func reveal(_ square: Square) {
+        guard square.isRevealed == false else { return }
+        guard square.isFlagged == false else { return }
+        square.isRevealed = true
+
+        if square.nearbyMines == 0 {
+            let adjacentSquares = getAdjacentSquares(toRow: square.row, column: square.column)
+
+            for adjacentSquare in adjacentSquares {
+                reveal(adjacentSquare)
+            }
+        }
+    }
+
+    func select(_ square: Square) {
+        guard square.isRevealed == false else { return }
+        guard square.isFlagged == false else { return }
+
+        // Place mines on first move.
+        if revealedSquares.count == 0 {
+            placeMines(avoiding: square)
+        }
+
+        if square.hasMine == false && square.nearbyMines == 0 {
+            // This square is empty = are there any other empty squares around ?
+            reveal(square)
+        } else {
+            square.isRevealed = true
+
+            if square.hasMine {
+                // You looose
+            }
+        }
+    }
+
+    func flag(_ square: Square) {
+        guard square.isRevealed == false else { return }
+        square.isFlagged.toggle()
     }
 }
 
